ba821a81586c6b6c696a2a89d82864f51660b5b2
ShrutikaJantre <81174919+ShrutikaJantre@users.noreply.github.com>
2022-05-23 11:43:09 +0530

    Add files via upload

---
+++ 220340520093/Section I.sql
@@ -0,0 +1,16 @@
+create table DEPT(
+DEPTNO int(2),
+DNAME varchar(15),
+LOC	varchar(10));
+
+create table emp(
+EMPNO int(4),
+ENAME varchar(10),
+JOB	varchar(9),
+HIREDATE date,
+SAL	float(7,2),
+COMM float(7,2),
+DEPTNO int(2)
+);
+
+
